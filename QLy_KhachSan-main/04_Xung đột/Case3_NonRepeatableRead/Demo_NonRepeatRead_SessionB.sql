225aa6853b2c0bc1bc87a5b337c83369821e5ae8
NhwNgocc <ngocttn5944@ut.edu.vn>
2026-09-15 05:03:26 +0700

    Delete tự động hóa waitlist

--- QLy_KhachSan-main/04_Xung đột/Case3_NonRepeatableRead/Demo_NonRepeatRead_SessionB.sql
+++
@@ -1,9 +0,0 @@
-﻿-- SESSION B: LỄ TÂN THAO TÁC CHECK-IN CÙNG LÚC
-PRINT N'Lễ tân thao tác Check-in, đổi trạng thái DP101 thành "Đang ở"...';
-
--- Thao tác cập nhật nhanh của Lễ tân
-UPDATE DATPHONG 
-SET TrangThai = N'Đang ở'
-WHERE MaDatPhong = 'DP101';
-
-PRINT N' Check-in thành công! Đã chốt (Commit) dữ liệu xuống DB.';
--- QLy_KhachSan-main/04_Xung đột/Case4_PhantomRead/Demo_PhantomRead_SessionB.sql
+++
@@ -1,8 +0,0 @@
-﻿PRINT N'Lễ tân chốt đơn đặt phòng mới cho khách...';
-
--- Dùng KH01 để tránh lỗi khóa ngoại
--- TongTien = 500000: giá trị minh họa, giữ nhất quán với 2 booking mồi ở HauTruong
-INSERT INTO DATPHONG (MaDatPhong, MaKH, MaNV, TongTien, TrangThai, Version)
-VALUES ('DP_03', 'KH01', 'NV01', 500000, N'Đã đặt', 1);
-
-PRINT N' Tạo booking DP_03 thành công!';
